088320ace0e8b954b5c8e8c29703da037896a98a
sulco <tomek.sulkowski@gmail.com>
2022-05-16 13:28:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ladle-pcjj9k
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ladle-pcjj9k)
---
+++ src/basic.stories.tsx
@@ -0,0 +1,2 @@
+export const Hello = () => <h2>Simple Story</h2>;
+export const Button = () => <button>Button</button>;
